3daa6c4fb2b893d2685aa0d9607d7896f00a639e
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2023-06-29 17:26:28 +0100

    Terraform code to deploy the application to AKS

---
+++ terraform/application/variables.tf
@@ -0,0 +1,17 @@
+variable "cluster" {}
+variable "namespace" {}
+variable "environment" {}
+variable "azure_credentials_json" { default = null }
+variable "azure_resource_prefix" {}
+variable "config_short" {}
+variable "service_name" {}
+variable "service_short" {}
+variable "deploy_azure_backing_services" { default = true }
+variable "dns_suffix" {}
+variable "docker_image_tag" {}
+
+locals {
+  docker_repository = "ghcr.io/dfe-digital/teacher-pay-calculator"
+
+  azure_credentials = try(jsondecode(var.azure_credentials_json), null)
+}
