0aad5b78186f132781ce5a0aefe390504d7f0a48
Giuseppe Lobraico <your@users.noreply.github.com>
2023-07-03 15:29:34 +0200

    Add pages for privacy, accessibility and tos
    Using markdown for the views as it's easier to amend in this case
    
    The pages are added to the footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def footer_links
+    {
+      'Privacy' => privacy_notice_path,
+      'Accessibility statement' => accessibility_statement_path,
+      'Terms and conditions' => terms_and_conditions_path,
+    }
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '.footer_links' do
+    subject { helper.footer_links}
+
+    let(:expected_links) do
+      {
+        'Privacy' => privacy_notice_path,
+        'Accessibility statement' => accessibility_statement_path,
+        'Terms and conditions' => terms_and_conditions_path,
+      }
+    end
+
+    it { is_expected.to eq(expected_links) }
+  end
+end
