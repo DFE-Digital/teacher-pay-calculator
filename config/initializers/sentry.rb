91039445d64335e3cc7e30e23cc32c89bbaf6fd0
Giuseppe Lobraico <your@users.noreply.github.com>
2023-07-03 17:55:00 +0200

    Code changes to use ENVIRONMENT_NAME

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.environment = Rails.env
+  config.environment = ENV.fetch('ENVIRONMENT_NAME', 'local')
   config.traces_sample_rate = 0.2
 end
