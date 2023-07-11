0f0c75db869b9ece0bfb78445a2d35e6e0f52f22
Giuseppe Lobraico <your@users.noreply.github.com>
2023-07-11 11:16:40 +0100

    Remove traces sample rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.environment = Rails.application.config.environment_name
-  config.traces_sample_rate = 0.2
+  config.traces_sample_rate = 0.0
 end
